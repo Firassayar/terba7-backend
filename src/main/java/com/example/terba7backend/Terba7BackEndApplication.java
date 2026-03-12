0030f75783c7baff4cc3a7c916bc22dba9100a44
Firas Sayar <sayarmed@gmail.com>
2026-03-13 00:23:18 +0100

    "Initial commit - Terba7 Backend"

---
+++ src/main/java/com/example/terba7backend/Terba7BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.terba7backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Terba7BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Terba7BackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/terba7backend/Terba7BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.terba7backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Terba7BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
